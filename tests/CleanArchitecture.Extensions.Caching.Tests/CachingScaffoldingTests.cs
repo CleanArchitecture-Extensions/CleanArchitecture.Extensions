e1dc77864debfc6c28c81e6be5c351fa5263b2b4
Reza Heidari <m.heidari988@gmail.com>
2026-01-01 21:36:26 +0300

    Update DI comments and remove scaffolding test
    Improved comments in DependencyInjectionExtensions for clarity on multitenancy caching. Removed the placeholder CachingScaffoldingTests as real tests will be added with future caching features.

--- tests/CleanArchitecture.Extensions.Caching.Tests/CachingScaffoldingTests.cs
+++
@@ -1,7 +0,0 @@
-namespace CleanArchitecture.Extensions.Caching.Tests;
-
-public class CachingScaffoldingTests
-{
-    [Fact(Skip = "Scaffolding placeholder - real tests will arrive with caching features.")]
-    public void Placeholder() { }
-}
